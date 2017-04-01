d856429bd4ab7ba8f0353fe4b3736ce567c3bb45
bornehill <ali.escamilla.navarro@gmail.com>
2017-04-01 13:00:01 -0600

    PetData project

---
+++ PetData/Pet/PetProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using AgroPET.Datos.Catalogos;
+using Agropet.Entidades.Seguridad;
+using Agropet.Entidades.Especial;
+using Agropet.Entidades.Consultas;
+
+namespace PetData.Pet
+{
+    internal class PetProvider
+    {
+      internal List<EntidadMenuWeb> GetMenuWeb(EntidadMenuWeb menu)
+      {
+        CatalogosExtDA catalogoGenerico = new CatalogosExtDA();
+        return catalogoGenerico.ObtenerMenuWeb(menu);
+      }
+
+      internal List<EntidadBannersWeb> GetBannersWeb(EntidadBannersWeb banner)
+      {
+        CatalogosExtDA catalogoGenerico = new CatalogosExtDA();
+        return catalogoGenerico.ObtenerBannersWeb(banner);
+      }
+
+    internal List<MenuArticulos> GetMenuArticulos(MenuArticulos articulo)
+    {
+      CatalogosExtDA catalogoGenerico = new CatalogosExtDA();
+      return catalogoGenerico.GetMenuArticulos(articulo);
+    }
+
+  }
+}
---
+++ PetData/Pet/PetService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agropet.Entidades.Seguridad;
+using Agropet.Entidades.Especial;
+using Agropet.Entidades.Consultas;
+
+namespace PetData.Pet
+{
+    public class PetService
+    {
+      public List<EntidadMenuWeb> GetMenuWeb(EntidadMenuWeb menu)
+      {
+        return new PetProvider().GetMenuWeb(menu);
+      }
+
+      public List<EntidadBannersWeb> GetBannersWeb(EntidadBannersWeb banner)
+      {
+        return new PetProvider().GetBannersWeb(banner);
+      }
+
+    public List<MenuArticulos> GetMenuArticulos(MenuArticulos articulo)
+    {
+      return new PetProvider().GetMenuArticulos(articulo);
+    }
+
+  }
+}
