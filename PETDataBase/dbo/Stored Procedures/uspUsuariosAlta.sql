ee707dd129e6ae6707dd933205e229c6977e6644
Jval <jvaldl00@gmail.com>
2017-05-20 00:16:00 -0500

    SP

---
+++ PETDataBase/dbo/Stored Procedures/uspUsuariosAlta.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[uspUsuariosAlta]
+(
+	@idperfil int,
+	@claveusr varchar(20),
+	@nombreusr varchar(100), 
+	@passwordusr varchar(20), 
+	@activo bit, 
+	@idusuariocreo int
+)
+AS
+BEGIN
+    
+	IF NOT EXISTS(SELECT * FROM [dbo].[tbusuarios] WHERE claveusr = @claveusr)
+	begin
+		INSERT INTO [dbo].[tbusuarios](idperfil, nombreusr, claveusr, passwordusr, activo, idusuariocreo, fechacreacion)
+		VALUES (@idperfil, @nombreusr, @claveusr, @passwordusr, @activo, @idusuariocreo, GETDATE())
+    end   
+	 
+END
