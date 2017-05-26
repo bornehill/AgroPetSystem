5651bf73c4f8be7b916b50213e74e903bfa09755
Jval <jvaldl00@gmail.com>
2017-05-25 21:05:51 -0500

    se sube SP

---
+++ PETDataBase/dbo/Stored Procedures/uspPerfiles_Consultar.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[uspPerfiles_Consultar](
+	@IdPerfil int = null, 
+	@Activo int = null 
+)
+AS
+BEGIN
+	SELECT idperfil, nombreperfil, fechacreacion, idusuariocreo, fechaultmodif, idusuarioultmodif, activo
+	FROM tbperfiles 
+	WHERE idperfil = isnull(@IdPerfil , idperfil)
+		AND activo = isnull(@Activo, activo) 
+END
