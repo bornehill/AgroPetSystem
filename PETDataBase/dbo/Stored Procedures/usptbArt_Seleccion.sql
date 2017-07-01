dfb2ec468190e466ae70a70b9422fee0f3b19903
Jval <jvaldl00@gmail.com>
2017-07-01 11:30:14 -0500

    se quita la funcion concat

--- PETDataBase/dbo/Stored Procedures/usptbArt_Seleccion.sql
+++ PETDataBase/dbo/Stored Procedures/usptbArt_Seleccion.sql
@@ -16,31 +16,31 @@ BEGIN
 	BEGIN
 		IF (@NombreArt != '')
 		BEGIN
-			SET @nom = CONCAT(' (A.Nombre LIKE CONCAT(''%'',''',@NombreArt,''',''%'')) '); 
+			SET @nom = ' (A.Nombre LIKE CONCAT(''%'','''+@NombreArt+''',''%'')) '
 			IF (@LineaId > 0)
 			BEGIN
-				SET @nom = CONCAT(@nom,' AND AMM.MenuId = ',@LineaId,' ');
+				SET @nom = @nom+' AND AMM.MenuId = '+@LineaId+' '
 			END
 		END
 		ELSE
 			BEGIN
 				IF (@LineaId > 0)
 				BEGIN
-					SET @nom = CONCAT(' AMM.MenuId = ',@LineaId,' ');
+					SET @nom = ' AMM.MenuId = '+@LineaId+' '
 				END
 			END
 	END		
-	SET @script = CONCAT('SELECT A.Id_Art, CA.Clave_Articulo AS cve_articulo, A.Nombre AS NombreArt, AMM.MenuId AS LineaId, MWP.Menu AS NombrePadre, NULL AS Existencia ',
-	'FROM tbarticulos_menu_marca AS AMM ',
-	'INNER JOIN tbArticulos AS A ',
-	'ON A.Id_Art = AMM.Id_Art ',
-	'INNER JOIN tbClaves_Articulos AS CA ',
-	'ON CA.Articulo_Id = A.Articulo_Id ',
-	'INNER JOIN tbmenuweb AS MW ',
-	'ON MW.MenuId = AMM.MenuId ',
-	'INNER JOIN tbmenuweb AS MWP ',
-	'ON MW.Padre = MWP.MenuId ',
-	'WHERE ', @nom);
+	SET @script = 'SELECT A.Id_Art, CA.Clave_Articulo AS cve_articulo, A.Nombre AS NombreArt, AMM.MenuId AS LineaId, MWP.Menu AS NombrePadre, NULL AS Existencia '+
+	'FROM tbarticulos_menu_marca AS AMM '+
+	'INNER JOIN tbArticulos AS A '+
+	'ON A.Id_Art = AMM.Id_Art '+
+	'INNER JOIN tbClaves_Articulos AS CA '+
+	'ON CA.Articulo_Id = A.Articulo_Id '+
+	'INNER JOIN tbmenuweb AS MW '+
+	'ON MW.MenuId = AMM.MenuId '+
+	'INNER JOIN tbmenuweb AS MWP '+
+	'ON MW.Padre = MWP.MenuId '+
+	'WHERE '+ @nom
 
-	EXEC sp_executesql @script;
+	EXEC @script;
 END
